b57b489a1b8f7c470f6465df295506bc490c9ac7
Guusje Delsing <guusjedelsing@guusjes-air.kpn>
2023-04-17 20:55:24 +0200

    Added static pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
-    # path for about view
+    path(route='about', view=views.about, name='about'),
 
-    # path for contact us view
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
 
@@ -20,6 +20,7 @@ urlpatterns = [
     # path for logout
 
     path(route='', view=views.get_dealerships, name='index'),
+    path(route='static', view=views.get_static, name='static'),
 
     # path for dealer reviews view
 

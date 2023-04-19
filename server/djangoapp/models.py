83e928ada23242fdb2669fe9c80121e7165074e7
Guusje Delsing <guusjedelsing@guusjes-air.kpn>
2023-04-19 19:58:11 +0200

    added Django models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,8 +9,14 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30, default='john')
+    description = models.CharField(null=False, max_length=100, default='')
 
-
+    def __str__(self):
+        return "Name: " + self.name + ", " + \
+               "Description: " + self.description
+    
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
@@ -19,6 +25,33 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+
+    AUDI = 'Audi'
+    VOLKSWAGEN = 'Volkswagen'
+    KIA = 'Kia'
+    BMW = 'BMW'
+    TESLA = 'Tesla'
+    CARMAKE_CHOICES = [
+        (KIA, 'Kia'),
+        (AUDI, 'Audi'),
+        (VOLKSWAGEN, 'Volkswagen'),
+        (BMW, 'BMW'),
+        (TESLA, 'Tesla')
+    ]
+
+    name = models.CharField(null=False, max_length=30, default='john')
+    dealer_id = models.IntegerField(null=False)
+    type = models.CharField(null=False, max_length=20, choices=CARMAKE_CHOICES)
+    year = models.DateField(null=False)
+    carmake = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Name: " + self.name + ", " + \
+               "Dealer id: " + str(self.dealer_id) + "," + \
+               "Type:" + self.type + "," + \
+               "Year:" + str(self.year)
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -82,10 +82,32 @@ def registration_request(request):
 
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
+# def get_dealerships(request):
+#     context = {}
+#     if request.method == "GET":
+#         return render(request, 'djangoapp/index.html', context)
+    
 def get_dealerships(request):
-    context = {}
+
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+
+        context = {}
+
+        st = request.GET.get("st")
+        dealerId = request.GET.get("dealerId")
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/7ccc880f-504c-4f24-a816-b01352454616/dealership-package/get-dealership"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+
+        if st:
+            dealerships = get_dealers_from_cf(url, st=st)
+        elif dealerId:
+            dealerId = int(dealerId)
+            dealerships = get_dealers_from_cf(url, dealerId=dealerId)
+
+        context["dealership_list"] = dealerships
+
+        return render(request, "djangoapp/index.html", context=context)
     
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_static(request):
